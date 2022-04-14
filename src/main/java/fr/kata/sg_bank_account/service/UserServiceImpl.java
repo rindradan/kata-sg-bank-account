4c46ffed5cc15edf6299dd3d8350590bbfea51aa
rindra <rindra.randriambololona@gmail.com>
2022-04-14 21:52:22 +0200

    refactor: restructure user service

--- src/main/java/fr/kata/sg_bank_account/service/UserServiceImpl.java
+++ src/main/java/fr/kata/sg_bank_account/service/UserServiceImpl.java
@@ -3,13 +3,16 @@ package fr.kata.sg_bank_account.service;
 import fr.kata.sg_bank_account.exception.UserNotFoundException;
 import fr.kata.sg_bank_account.model.User;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
 public class UserServiceImpl implements UserService {
 
-    private final Map<UUID,User> users = new HashMap<>();
+    private final Map<UUID,User> users;
+
+    public UserServiceImpl(Map<UUID, User> users) {
+        this.users = users;
+    }
 
     @Override
     public User getUser(UUID id) throws UserNotFoundException {
@@ -17,10 +20,5 @@ public class UserServiceImpl implements UserService {
             return users.get(id);
         }
         throw new UserNotFoundException("User with UUID: "+ id + " not found!");
-    }
-
-    @Override
-    public Map<UUID, User> getUserMap() {
-        return users;
     }
 }
--- src/test/java/fr/kata/sg_bank_account/service/TestUserService.java
+++ src/test/java/fr/kata/sg_bank_account/service/TestUserService.java
@@ -3,17 +3,17 @@ package fr.kata.sg_bank_account.service;
 import fr.kata.sg_bank_account.exception.UserNotFoundException;
 import org.junit.jupiter.api.Test;
 
+import java.util.Collections;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-public class TestUserService {
+class TestUserService {
 
     @Test
     void should_get_single_user() throws UserNotFoundException {
-        var userService = new UserServiceImpl();
-        userService.getUserMap().putAll(TestUserServiceData.generateUsers());
+        var userService = new UserServiceImpl(TestUserServiceData.generateUsers());
 
         var uuid = UUID.fromString("dd8d795c-b980-11ec-8422-0242ac120002");
         var user = userService.getUser(uuid);
@@ -23,6 +23,6 @@ public class TestUserService {
 
     @Test
     void should_fail_when_user_not_found() {
-        assertThrows(UserNotFoundException.class, () -> new UserServiceImpl().getUser(UUID.randomUUID()));
+        assertThrows(UserNotFoundException.class, () -> new UserServiceImpl(Collections.emptyMap()).getUser(UUID.randomUUID()));
     }
 }
